572026848305738be7456ed034bf4af12e548f4b
Filip Farag <fil.farag@gmail.com>
2025-05-08 16:20:55 +0200

    Finish step 2

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-const addVoucher = document.querySelector(".basket-price__add-voucher");
-const removeVoucher = document.querySelector(".basket-price__remove-voucher");
-const voucherInput = document.querySelector(".basket-price__input");
-const voucherButton = document.querySelector(".basket-price__button");
+const addItem = document.querySelector(".pop-basket__add");
+const removeItem = document.querySelector(".pop-basket__remove");
 
-addVoucher.addEventListener("click", function () {
-  addVoucher.style.display = "none";
-  removeVoucher.style.display = "flex";
-  voucherInput.style.display = "initial";
-  voucherButton.style.display = "initial";
+const voucherInputs = document.querySelectorAll(".basket-price__input");
+const voucherButtons = document.querySelectorAll(".basket-price__button");
+const basketForms = document.querySelectorAll(".basket-form__billing");
+
+addItem.addEventListener("click", function () {
+  addItem.style.display = "none";
+  removeItem.style.display = "flex";
+
+  basketForms.forEach((form) => (form.style.display = "grid"));
+  voucherInputs.forEach((input) => (input.style.display = "initial"));
+  voucherButtons.forEach((button) => (button.style.display = "initial"));
 });
 
-removeVoucher.addEventListener("click", function () {
-  addVoucher.style.display = "flex";
-  removeVoucher.style.display = "none";
-  voucherInput.style.display = "";
-  voucherButton.style.display = "";
+removeItem.addEventListener("click", function () {
+  addItem.style.display = "flex";
+  removeItem.style.display = "none";
+
+  basketForms.forEach((form) => (form.style.display = "none"));
+  voucherInputs.forEach((input) => (input.style.display = ""));
+  voucherButtons.forEach((button) => (button.style.display = ""));
 });
